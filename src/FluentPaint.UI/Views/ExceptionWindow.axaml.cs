cc39df23edd1f938d6ab9cb11e4310d61506373e
Bazalii Ivan <bazalijivan2002@yandex.ru>
2022-10-14 11:06:29 +0300

    feat: realise ExceptionWindow

---
+++ src/FluentPaint.UI/Views/ExceptionWindow.axaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace FluentPaint.UI.Views;
+
+public partial class ExceptionWindow : Window
+{
+    public ExceptionWindow()
+    {
+        InitializeComponent();
+
+#if DEBUG
+        this.AttachDevTools();
+#endif
+    }
+
+    private void InitializeComponent()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+}
