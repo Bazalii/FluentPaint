180ef5dad01356adacf503d80d968f70b595df54
Bazalii Ivan <bazalijivan2002@yandex.ru>
2022-10-14 10:59:49 +0300

    feat: realise SavePopupWindow

---
+++ src/FluentPaint.UI/Views/SavePopupWindow.axaml.cs
@@ -0,0 +1,33 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonia.Markup.Xaml;
+
+namespace FluentPaint.UI.Views;
+
+public partial class SavePopupWindow : Window
+{
+    public SavePopupWindow()
+    {
+        InitializeComponent();
+
+#if DEBUG
+        this.AttachDevTools();
+#endif
+    }
+
+    private void InitializeComponent()
+    {
+        AvaloniaXamlLoader.Load(this);
+#if DEBUG
+        this.AttachDevTools();
+#endif
+    }
+
+    private void OnOkButtonClickCommand(object sender, RoutedEventArgs e)
+    {
+        var userInput = this.FindControl<TextBox>("SaveFilePath").Text;
+
+        Close(userInput);
+    }
+}
