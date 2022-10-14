aab4eb26eb99a99ae0941e228c3d5173ec722f80
Bazalii Ivan <bazalijivan2002@yandex.ru>
2022-10-14 11:13:25 +0300

    feat: assign SKBitmapExtensions file

---
+++ src/FluentPaint.UI/SKBitmapExtensions.cs
@@ -0,0 +1,12 @@
+﻿using SkiaSharp;
+using Bitmap = Avalonia.Media.Imaging.Bitmap;
+
+namespace FluentPaint.UI;
+
+public static class SKBitmapExtensions
+{
+    public static Bitmap ConvertToAvaloniaBitmap(this SKBitmap bitmap)
+    {
+        return new Bitmap("");
+    }
+}
