c24bf9e82f5dbe7f43275ad69ca8bd6d03d844ed
Bazalii Ivan <bazalijivan2002@yandex.ru>
2022-10-14 11:24:37 +0300

    feat: realise MainWindowViewModel

---
+++ src/FluentPaint.UI/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,18 @@
+﻿using ReactiveUI;
+using SkiaSharp;
+
+namespace FluentPaint.UI.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        private SKBitmap _file = new();
+
+        public string FilePath { get; set; } = string.Empty;
+
+        public SKBitmap File
+        {
+            get => _file;
+            private set => this.RaiseAndSetIfChanged(ref _file, value);
+        }
+    }
+}
