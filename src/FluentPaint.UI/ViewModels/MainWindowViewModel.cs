f0e95595108b73e13e154da0e9266bbf423fb520
Bazalii Ivan <bazalijivan2002@yandex.ru>
2022-10-28 00:39:35 +0300

    feat: improve MainWindowViewModel
    - Add backing fields for channels choosing
    - Realise GetBitmapChannels method
    - Realise SetChannels method

--- src/FluentPaint.UI/ViewModels/MainWindowViewModel.cs
+++ src/FluentPaint.UI/ViewModels/MainWindowViewModel.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using Avalonia.Controls;
+using FluentPaint.Core.Channels;
+using FluentPaint.Core.Channels.Implementations;
 using FluentPaint.Core.Converters;
+using FluentPaint.Core.Enums;
 using FluentPaint.Core.Pnm;
 using ReactiveUI;
 using SkiaSharp;
@@ -12,19 +15,25 @@ namespace FluentPaint.UI.ViewModels
     {
         private string _loadingFilePath = string.Empty;
         private string _savingFilePath = string.Empty;
+
         private string _selectedSpace = "Space choosing";
 
         private SKBitmap? _rgbFile;
         private SKBitmap _currentColorSpaceFile = new();
 
         private readonly ConverterFactory _converterFactory = new();
+        private readonly IChannelGetter _channelGetter = new ChannelGetter();
 
         private readonly List<string> _colorSpaceNames = new()
             { "RGB", "HSL", "HSV", "YCbCr601", "YCbCr709", "YCoCg", "CMY" };
 
+        private readonly List<string> _channels = new()
+            { "First", "Second", "Third", "FirstAndSecond", "FirstAndThird", "SecondAndThird", "All" };
+
         public MainWindowViewModel()
         {
             SetColorSpaces();
+            SetChannels();
         }
 
         public string LoadingFilePath
@@ -56,7 +65,7 @@ namespace FluentPaint.UI.ViewModels
             set
             {
                 _savingFilePath = value;
-                
+
                 Enum.TryParse(SelectedSpace, out ColorSpace colorSpace);
 
                 Pnm.WritePnm(_savingFilePath, colorSpace == ColorSpace.RGB ? _rgbFile : _currentColorSpaceFile);
@@ -67,9 +76,11 @@ namespace FluentPaint.UI.ViewModels
         {
             get => _rgbFile;
             set => this.RaiseAndSetIfChanged(ref _rgbFile, value);
-    }
+        }
 
-        public List<ComboBoxItem> Items { get; set; } = new();
+        public List<ComboBoxItem> Spaces { get; set; } = new();
+
+        public List<ComboBoxItem> Channels { get; set; } = new();
 
         public string SelectedSpace
         {
@@ -83,10 +94,29 @@ namespace FluentPaint.UI.ViewModels
                 Enum.TryParse(SelectedSpace, out ColorSpace colorSpace);
 
                 if (colorSpace == ColorSpace.RGB) return;
-                
+
                 var convertor = _converterFactory.GetConverter(colorSpace);
                 _currentColorSpaceFile = convertor.FromRgb(_rgbFile);
             }
+        }
+
+        public string SelectedChannels { get; set; } = "Channel";
+
+        public SKBitmap GetBitmapChannels()
+        {
+            Enum.TryParse(SelectedSpace, out ColorSpace colorSpace);
+            Enum.TryParse(SelectedChannels, out ColorChannels colorChannels);
+
+            if (colorSpace == ColorSpace.RGB)
+            {
+                return _channelGetter.GetChannels(_rgbFile, colorChannels);
+            }
+
+            var convertor = _converterFactory.GetConverter(colorSpace);
+
+            var modifiedCurrentColorSpaceBitmap = _channelGetter.GetChannels(_currentColorSpaceFile, colorChannels);
+
+            return convertor.ToRgb(modifiedCurrentColorSpaceBitmap);
         }
 
         private void SetColorSpaces()
@@ -98,7 +128,20 @@ namespace FluentPaint.UI.ViewModels
                     Content = name
                 };
 
-                Items.Add(comboBoxItem);
+                Spaces.Add(comboBoxItem);
+            });
+        }
+
+        private void SetChannels()
+        {
+            _channels.ForEach(name =>
+            {
+                var comboBoxItem = new ComboBoxItem
+                {
+                    Content = name
+                };
+
+                Channels.Add(comboBoxItem);
             });
         }
     }
