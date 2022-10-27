2ec6bc308233cd99eb31c23d9fe3da007e5bedd5
t1mtg <timagurman@gmail.com>
2022-10-27 22:32:14 +0300

    fix: change formulas in converters

--- src/FluentPaint.Core/Converters/Implementations/YCbCr601Converter.cs
+++ src/FluentPaint.Core/Converters/Implementations/YCbCr601Converter.cs
@@ -4,13 +4,12 @@ namespace FluentPaint.Core.Converters.Implementations;
 
 public class YCbCr601Converter : IConverter
 {
-    
     private const float A = 0.299f;
     private const float B = 0.587f;
     private const float C = 0.114f;
     private const float D = 1.772f;
     private const float E = 1.402f;
-    
+
     public SKBitmap FromRgb(SKBitmap bitmap)
     {
         var convertedBitmap = new SKBitmap(bitmap.Width, bitmap.Height);
@@ -21,15 +20,34 @@ public class YCbCr601Converter : IConverter
             {
                 var pixel = bitmap.GetPixel(x, y);
 
-                var red = pixel.Red;
-                var blue = pixel.Blue;
-                var green = pixel.Green;
+                var red = pixel.Red / 255f;
+                var blue = pixel.Blue / 255f;
+                var green = pixel.Green / 255f;
 
-                var luminance = A * red + B * green + C * blue;
+                /*var luminance = A * red + B * green + C * blue;
                 var blueComponent = (blue - luminance) / D;
-                var redComponent = (red - luminance) / E;
+                var redComponent = (red - luminance) / E;*/
 
-                convertedBitmap.SetPixel(x, y, new SKColor((byte) luminance, (byte) blueComponent,(byte) redComponent));
+                var luminance = 16 + 65.481f * red + 128.553f * green + 24.996f * blue;
+                var blueComponent = 128 - 37.797f * red - 74.203 * green + 112.0f * blue;
+                var redComponent = 128 + 112.0f * red - 93.786f * green - 18.214f * blue;
+
+                /*blueComponent = blueComponent switch
+                {
+                    > 240 => 240,
+                    < 16 => 16,
+                    _ => blueComponent
+                };
+                
+                redComponent = redComponent switch
+                {
+                    > 240 => 240,
+                    < 16 => 16,
+                    _ => redComponent
+                };*/
+
+                convertedBitmap.SetPixel(x, y,
+                    new SKColor((byte) luminance, (byte) blueComponent, (byte) redComponent));
             }
         }
 
@@ -39,22 +57,46 @@ public class YCbCr601Converter : IConverter
     public SKBitmap ToRgb(SKBitmap bitmap)
     {
         var convertedBitmap = new SKBitmap(bitmap.Width, bitmap.Height);
-        
+
         for (var y = 0; y < bitmap.Height; y++)
         {
             for (var x = 0; x < bitmap.Width; x++)
             {
                 var pixel = bitmap.GetPixel(x, y);
 
-                var luminance = pixel.Red;
-                var blueComponent = pixel.Green;
-                var redComponent = pixel.Blue;
-
-                var red = luminance + E * redComponent;
-                var green = luminance - (A * E / B) * redComponent - (C * D / B) * blueComponent;
-                var blue = luminance + D * blueComponent;
+                var luminance = pixel.Red / 256f;
+                var blueComponent = pixel.Green / 256f;
+                var redComponent = pixel.Blue / 256f;
                 
-                convertedBitmap.SetPixel(x, y, new SKColor((byte) red,(byte) green,(byte) blue));
+                
+                /*blueComponent = blueComponent switch
+                {
+                    > 240 / 256f => 240 / 256f,
+                    < 16 / 256f => 16 / 256f,
+                    _ => blueComponent
+                };
+                
+                redComponent = redComponent switch
+                {
+                    > 240 / 256f=> 240/ 256f,
+                    < 16/ 256f => 16/ 256f,
+                    _ => redComponent
+                };*/
+
+                var red = 298.082f * luminance + 408.583f * redComponent - 222.921;
+                var green = 298.082f * luminance - 100.291f * blueComponent - 208.120f * redComponent + 135.576;
+                var blue = 298.082f * luminance + 516.412f * blueComponent - 276.836;
+
+                /*var red = (255 / 219) * (luminance - 16) + 255f / 224 * 1.402f * (redComponent - 128);
+
+                var green = (255 / 219) * (luminance - 16) - (255f / 214) * 1.772f * (0.114f / 0.587f) *
+                                                           (blueComponent - 128)
+                                                           - (255f / 214) * 1.402f * (0.299f / 0.587) *
+                                                           (redComponent - 128);
+
+                var blue = (255 / 219) * (luminance - 16) + (255f / 214) * 1.772f * (blueComponent - 128);*/
+
+                convertedBitmap.SetPixel(x, y, new SKColor((byte) red, (byte) green, (byte) blue));
             }
         }
 
--- src/FluentPaint.Core/Converters/Implementations/YCbCr709Converter.cs
+++ src/FluentPaint.Core/Converters/Implementations/YCbCr709Converter.cs
@@ -1,10 +1,11 @@
+using MathNet.Numerics.LinearAlgebra;
+using MathNet.Numerics.LinearAlgebra.Double;
 using SkiaSharp;
 
 namespace FluentPaint.Core.Converters.Implementations;
 
 public class YCbCr709Converter : IConverter
 {
-    
     private const float A = 0.2126f;
     private const float B = 0.7152f;
     private const float C = 0.0752f;
@@ -21,16 +22,35 @@ public class YCbCr709Converter : IConverter
             {
                 var pixel = bitmap.GetPixel(x, y);
 
-                var red = pixel.Red;
-                var blue = pixel.Blue;
-                var green = pixel.Green;
+                var red = pixel.Red / 255f;
+                var blue = pixel.Blue / 255f;
+                var green = pixel.Green / 255f;
 
-                var luminance = A * red + B * green + C * blue;
-                var blueComponent = (blue - luminance) / D;
-                var redComponent = (red - luminance) / E;
+                Matrix<double> a = Matrix<double>.Build.Dense(3, 3);
+                Vector<double> v = Vector<double>.Build.Dense(3);
+
+                v[0] = red;
+                v[1] = green;
+                v[2] = blue;
+
+                a[0, 0] = 0.2126;
+                a[0, 1] = 0.7152;
+                a[0, 2] = 0.0722;
+                a[1, 0] = -0.1146;
+                a[1, 1] = -0.3854;
+                a[1, 2] = 0.5;
+                a[2, 0] = 0.5;
+                a[2, 1] = -0.4542;
+                a[2, 2] = -0.0458;
+
+                var result = a * v;
+
+                var luminance = result[0];
+                var blueComponent = result[1];
+                var redComponent = result[2];
 
                 convertedBitmap.SetPixel(x, y,
-                    new SKColor((byte) luminance, (byte) blueComponent, (byte) redComponent));
+                    new SKColor((byte) (luminance * 255), (byte) (blueComponent * 255), (byte) (redComponent * 255)));
             }
         }
 
@@ -46,15 +66,35 @@ public class YCbCr709Converter : IConverter
             {
                 var pixel = bitmap.GetPixel(x, y);
 
-                var luminance = pixel.Red;
-                var blueComponent = pixel.Green;
-                var redComponent = pixel.Blue;
+                var luminance = pixel.Red/ 255f;
+                var blueComponent = pixel.Green/ 255f;
+                var redComponent = pixel.Blue/ 255f;
 
-                var red = luminance + E * redComponent;
-                var green = luminance - (A * E / B) * redComponent - (C * D / B) * blueComponent;
-                var blue = luminance + D * blueComponent;
 
-                convertedBitmap.SetPixel(x, y, new SKColor((byte) red, (byte) green, (byte) blue));
+                var a = Matrix<double>.Build.Dense(3, 3);
+                var v = Vector<double>.Build.Dense(3);
+
+                v[0] = luminance;
+                v[1] = blueComponent;
+                v[2] = redComponent;
+
+                a[0, 0] = 1;
+                a[0, 1] = 0;
+                a[0, 2] = 1.5748;
+                a[1, 0] = 1;
+                a[1, 1] = -0.1873;
+                a[1, 2] = -0.4681;
+                a[2, 0] = 1;
+                a[2, 1] = 1.8556;
+                a[2, 2] = 0;
+
+                var result = a * v;
+
+                var red = result[0];
+                var green = result[1];
+                var blue = result[2];
+
+                convertedBitmap.SetPixel(x, y, new SKColor((byte) (red * 255), (byte) (green * 255), (byte) (blue * 255)));
             }
         }
 
