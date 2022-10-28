a0ee925573c4d46afdbd698a114d3f1c70d24152
Bazalii Ivan <bazalijivan2002@yandex.ru>
2022-10-28 23:15:29 +0300

    refactor: add usage of file-scoped namespaces

--- src/FluentPaint.UI/Program.cs
+++ src/FluentPaint.UI/Program.cs
@@ -2,38 +2,37 @@
 using Avalonia.ReactiveUI;
 using System;
 
-namespace FluentPaint.UI
+namespace FluentPaint.UI;
+
+/// <summary>
+/// The entry point for application startup.
+/// </summary>
+class Program
 {
     /// <summary>
-    /// The entry point for application startup.
+    /// Initialization code.
     /// </summary>
-    class Program
-    {
-        /// <summary>
-        /// Initialization code.
-        /// </summary>
-        /// <remarks>
-        /// Don't use any Avalonia, third-party APIs or any SynchronizationContext-reliant code
-        /// before AppMain is called: things aren't initialized yet and stuff might break.
-        /// </remarks>
-        /// <param name="args"> Program arguments</param>
-        [STAThread]
-        public static void Main(string[] args) => BuildAvaloniaApp()
-            .StartWithClassicDesktopLifetime(args);
+    /// <remarks>
+    /// Don't use any Avalonia, third-party APIs or any SynchronizationContext-reliant code
+    /// before AppMain is called: things aren't initialized yet and stuff might break.
+    /// </remarks>
+    /// <param name="args"> Program arguments</param>
+    [STAThread]
+    public static void Main(string[] args) => BuildAvaloniaApp()
+        .StartWithClassicDesktopLifetime(args);
 
-        /// <summary>
-        /// Avalonia configuration
-        /// </summary>
-        /// <remarks>
-        /// Also used by visual designer
-        /// </remarks>
-        /// <returns>
-        /// <see cref="AppBuilder"/>
-        /// </returns>
-        public static AppBuilder BuildAvaloniaApp()
-            => AppBuilder.Configure<App>()
-                .UsePlatformDetect()
-                .LogToTrace()
-                .UseReactiveUI();
-    }
+    /// <summary>
+    /// Avalonia configuration
+    /// </summary>
+    /// <remarks>
+    /// Also used by visual designer
+    /// </remarks>
+    /// <returns>
+    /// <see cref="AppBuilder"/>
+    /// </returns>
+    public static AppBuilder BuildAvaloniaApp()
+        => AppBuilder.Configure<App>()
+            .UsePlatformDetect()
+            .LogToTrace()
+            .UseReactiveUI();
 }
--- src/FluentPaint.UI/ViewModels/MainWindowViewModel.cs
+++ src/FluentPaint.UI/ViewModels/MainWindowViewModel.cs
@@ -9,185 +9,184 @@ using FluentPaint.Core.Pnm;
 using ReactiveUI;
 using SkiaSharp;
 
-namespace FluentPaint.UI.ViewModels
+namespace FluentPaint.UI.ViewModels;
+
+/// <summary>
+/// Contains business logic and data for MainWindow view.
+/// </summary>
+public class MainWindowViewModel : ReactiveObject
 {
-    /// <summary>
-    /// Contains business logic and data for MainWindow view.
-    /// </summary>
-    public class MainWindowViewModel : ReactiveObject
+    private string _loadingFilePath = string.Empty;
+    private string _savingFilePath = string.Empty;
+
+    private string _selectedSpace = "Space choosing";
+
+    private SKBitmap? _rgbFile;
+    private SKBitmap _currentColorSpaceFile = new();
+
+    private readonly ConverterFactory _converterFactory = new();
+    private readonly IChannelGetter _channelGetter = new ChannelGetter();
+
+    private readonly List<string> _colorSpaceNames = new()
+        { "RGB", "HSL", "HSV", "YCbCr601", "YCbCr709", "YCoCg", "CMY" };
+
+    private readonly List<string> _channels = new()
+        { "First", "Second", "Third", "FirstAndSecond", "FirstAndThird", "SecondAndThird", "All" };
+
+    public MainWindowViewModel()
     {
-        private string _loadingFilePath = string.Empty;
-        private string _savingFilePath = string.Empty;
+        SetColorSpaces();
+        SetChannels();
+    }
 
-        private string _selectedSpace = "Space choosing";
-
-        private SKBitmap? _rgbFile;
-        private SKBitmap _currentColorSpaceFile = new();
-
-        private readonly ConverterFactory _converterFactory = new();
-        private readonly IChannelGetter _channelGetter = new ChannelGetter();
-
-        private readonly List<string> _colorSpaceNames = new()
-            { "RGB", "HSL", "HSV", "YCbCr601", "YCbCr709", "YCoCg", "CMY" };
-
-        private readonly List<string> _channels = new()
-            { "First", "Second", "Third", "FirstAndSecond", "FirstAndThird", "SecondAndThird", "All" };
-
-        public MainWindowViewModel()
+    /// <summary>
+    /// Path to the file that will be loaded in the application.
+    /// </summary>
+    /// <remarks>
+    /// When this property is changed loads file in RGB color space.
+    /// </remarks>
+    public string LoadingFilePath
+    {
+        get => _loadingFilePath;
+        set
         {
-            SetColorSpaces();
-            SetChannels();
-        }
+            _loadingFilePath = value;
+            var file = Pnm.ReadPnm(value);
 
-        /// <summary>
-        /// Path to the file that will be loaded in the application.
-        /// </summary>
-        /// <remarks>
-        /// When this property is changed loads file in RGB color space.
-        /// </remarks>
-        public string LoadingFilePath
-        {
-            get => _loadingFilePath;
-            set
-            {
-                _loadingFilePath = value;
-                var file = Pnm.ReadPnm(value);
-
-                Enum.TryParse(SelectedSpace, out ColorSpace colorSpace);
-
-                if (colorSpace == ColorSpace.RGB)
-                {
-                    RgbFile = file;
-                }
-                else
-                {
-                    var convertor = _converterFactory.GetConverter(colorSpace);
-
-                    RgbFile = convertor.ToRgb(file);
-                }
-            }
-        }
-
-        /// <summary>
-        /// Path to the file in which current picture will be saved in chosen color space.
-        /// </summary>
-        /// <remarks>
-        /// When this property is changed also saves file in in chosen color space.
-        /// </remarks>
-        public string SavingFilePath
-        {
-            get => _savingFilePath;
-            set
-            {
-                _savingFilePath = value;
-
-                Enum.TryParse(SelectedSpace, out ColorSpace colorSpace);
-
-                Pnm.WritePnm(_savingFilePath, colorSpace == ColorSpace.RGB ? _rgbFile : _currentColorSpaceFile);
-            }
-        }
-
-        /// <summary>
-        /// Represents loaded file in RGB color space.
-        /// </summary>
-        public SKBitmap RgbFile
-        {
-            get => _rgbFile;
-            set => this.RaiseAndSetIfChanged(ref _rgbFile, value);
-        }
-
-        /// <summary>
-        /// Items that are displayed in ColorSpaceChoosing ComboBox of MainWindow.
-        /// </summary>
-        public List<ComboBoxItem> Spaces { get; set; } = new();
-
-        /// <summary>
-        /// Items that are displayed in ChannelsChoosing ComboBox of MainWindow.
-        /// </summary>
-        public List<ComboBoxItem> Channels { get; set; } = new();
-
-        /// <summary>
-        /// Color space that is selected by the user.
-        /// </summary>
-        /// <remarks>
-        /// When this property is changed also converts file to new color space.
-        /// </remarks>
-        public string SelectedSpace
-        {
-            get => _selectedSpace;
-            set
-            {
-                _selectedSpace = value;
-
-                if (_rgbFile is null) return;
-
-                Enum.TryParse(SelectedSpace, out ColorSpace colorSpace);
-
-                if (colorSpace == ColorSpace.RGB) return;
-
-                var convertor = _converterFactory.GetConverter(colorSpace);
-                _currentColorSpaceFile = convertor.FromRgb(_rgbFile);
-            }
-        }
-
-        /// <summary>
-        /// Color channels that will be shown.
-        /// </summary>
-        public string SelectedChannels { get; set; } = "Channel";
-
-        /// <summary>
-        /// Filters image in current color space so that output contains only chosen channels using <see cref="SelectedSpace"/> and <see cref="SelectedChannels"/>.
-        /// </summary>
-        /// <returns>
-        /// Bitmap that contains only chosen channels.
-        /// </returns>
-        public SKBitmap GetBitmapChannels()
-        {
             Enum.TryParse(SelectedSpace, out ColorSpace colorSpace);
-            Enum.TryParse(SelectedChannels, out ColorChannels colorChannels);
 
             if (colorSpace == ColorSpace.RGB)
             {
-                return _channelGetter.GetChannels(_rgbFile, colorChannels);
+                RgbFile = file;
             }
+            else
+            {
+                var convertor = _converterFactory.GetConverter(colorSpace);
+
+                RgbFile = convertor.ToRgb(file);
+            }
+        }
+    }
+
+    /// <summary>
+    /// Path to the file in which current picture will be saved in chosen color space.
+    /// </summary>
+    /// <remarks>
+    /// When this property is changed also saves file in in chosen color space.
+    /// </remarks>
+    public string SavingFilePath
+    {
+        get => _savingFilePath;
+        set
+        {
+            _savingFilePath = value;
+
+            Enum.TryParse(SelectedSpace, out ColorSpace colorSpace);
+
+            Pnm.WritePnm(_savingFilePath, colorSpace == ColorSpace.RGB ? _rgbFile : _currentColorSpaceFile);
+        }
+    }
+
+    /// <summary>
+    /// Represents loaded file in RGB color space.
+    /// </summary>
+    public SKBitmap RgbFile
+    {
+        get => _rgbFile;
+        set => this.RaiseAndSetIfChanged(ref _rgbFile, value);
+    }
+
+    /// <summary>
+    /// Items that are displayed in ColorSpaceChoosing ComboBox of MainWindow.
+    /// </summary>
+    public List<ComboBoxItem> Spaces { get; set; } = new();
+
+    /// <summary>
+    /// Items that are displayed in ChannelsChoosing ComboBox of MainWindow.
+    /// </summary>
+    public List<ComboBoxItem> Channels { get; set; } = new();
+
+    /// <summary>
+    /// Color space that is selected by the user.
+    /// </summary>
+    /// <remarks>
+    /// When this property is changed also converts file to new color space.
+    /// </remarks>
+    public string SelectedSpace
+    {
+        get => _selectedSpace;
+        set
+        {
+            _selectedSpace = value;
+
+            if (_rgbFile is null) return;
+
+            Enum.TryParse(SelectedSpace, out ColorSpace colorSpace);
+
+            if (colorSpace == ColorSpace.RGB) return;
 
             var convertor = _converterFactory.GetConverter(colorSpace);
-
-            var modifiedCurrentColorSpaceBitmap = _channelGetter.GetChannels(_currentColorSpaceFile, colorChannels);
-
-            return convertor.ToRgb(modifiedCurrentColorSpaceBitmap);
+            _currentColorSpaceFile = convertor.FromRgb(_rgbFile);
         }
+    }
 
-        /// <summary>
-        /// Adds <see cref="ComboBoxItem"/> for each color space name in <see cref="Spaces"/> that are displayed in MainWindow.
-        /// </summary>
-        private void SetColorSpaces()
+    /// <summary>
+    /// Color channels that will be shown.
+    /// </summary>
+    public string SelectedChannels { get; set; } = "Channel";
+
+    /// <summary>
+    /// Filters image in current color space so that output contains only chosen channels using <see cref="SelectedSpace"/> and <see cref="SelectedChannels"/>.
+    /// </summary>
+    /// <returns>
+    /// Bitmap that contains only chosen channels.
+    /// </returns>
+    public SKBitmap GetBitmapChannels()
+    {
+        Enum.TryParse(SelectedSpace, out ColorSpace colorSpace);
+        Enum.TryParse(SelectedChannels, out ColorChannels colorChannels);
+
+        if (colorSpace == ColorSpace.RGB)
         {
-            _colorSpaceNames.ForEach(name =>
-            {
-                var comboBoxItem = new ComboBoxItem
-                {
-                    Content = name
-                };
-
-                Spaces.Add(comboBoxItem);
-            });
+            return _channelGetter.GetChannels(_rgbFile, colorChannels);
         }
 
-        /// <summary>
-        /// Adds <see cref="ComboBoxItem"/> for each channels combination in <see cref="Channels"/> that are displayed in MainWindow.
-        /// </summary>
-        private void SetChannels()
+        var convertor = _converterFactory.GetConverter(colorSpace);
+
+        var modifiedCurrentColorSpaceBitmap = _channelGetter.GetChannels(_currentColorSpaceFile, colorChannels);
+
+        return convertor.ToRgb(modifiedCurrentColorSpaceBitmap);
+    }
+
+    /// <summary>
+    /// Adds <see cref="ComboBoxItem"/> for each color space name in <see cref="Spaces"/> that are displayed in MainWindow.
+    /// </summary>
+    private void SetColorSpaces()
+    {
+        _colorSpaceNames.ForEach(name =>
         {
-            _channels.ForEach(name =>
+            var comboBoxItem = new ComboBoxItem
             {
-                var comboBoxItem = new ComboBoxItem
-                {
-                    Content = name
-                };
+                Content = name
+            };
 
-                Channels.Add(comboBoxItem);
-            });
-        }
+            Spaces.Add(comboBoxItem);
+        });
+    }
+
+    /// <summary>
+    /// Adds <see cref="ComboBoxItem"/> for each channels combination in <see cref="Channels"/> that are displayed in MainWindow.
+    /// </summary>
+    private void SetChannels()
+    {
+        _channels.ForEach(name =>
+        {
+            var comboBoxItem = new ComboBoxItem
+            {
+                Content = name
+            };
+
+            Channels.Add(comboBoxItem);
+        });
     }
 }
