86203785608e735abe92f365fa14f84182e6900e
FuchsbauFox <vovasai@mail.ru>
2022-10-29 16:25:28 +0300

    feat: write comments for Readers

--- src/FluentPaint.Core/Reader/IPictureReader.cs
+++ src/FluentPaint.Core/Reader/IPictureReader.cs
@@ -2,7 +2,17 @@ using SkiaSharp;
 
 namespace FluentPaint.Core.Reader;
 
+/// <summary>
+/// Default interface for readers.
+/// </summary>
 public interface IPictureReader
 {
+    /// <summary>
+    /// Read picture from file
+    /// </summary>
+    /// <param name="fileStream"> file with picture</param>
+    /// <param name="width"> picture width, the number of bytes in the row</param>
+    /// <param name="height"> picture height, the number of bytes in the column</param>
+    /// <returns>filled bitmap</returns>
     SKBitmap ReadImageData(FileStream fileStream, int width, int height);
 }
