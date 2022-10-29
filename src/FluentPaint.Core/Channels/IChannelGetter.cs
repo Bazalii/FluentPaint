1366d6ab0868bc73d3e28ed101432b48f0cf6b76
t1mtg <timagurman@gmail.com>
2022-10-29 19:32:41 +0300

    docs: add comments for channel getter

--- src/FluentPaint.Core/Channels/IChannelGetter.cs
+++ src/FluentPaint.Core/Channels/IChannelGetter.cs
@@ -3,7 +3,18 @@ using FluentPaint.Core.Enums;
 
 namespace FluentPaint.Core.Channels;
 
+/// <summary>
+/// Provides method to get one or many color channels of pictures.
+/// </summary>
 public interface IChannelGetter
 {
+    /// <summary>
+    /// Get picture in provided color channels.
+    /// </summary>
+    /// <param name="bitmap"> <see cref="SKBitmap"/> Bitmap containing all pixels of the picture. </param>
+    /// <param name="channels"> Channels in which the image will be displayed. </param>
+    /// <returns>
+    /// Bitmap with pixels with correct color according to provided channels.
+    /// </returns>
     SKBitmap GetChannels(SKBitmap bitmap, ColorChannels channels);
 }
--- src/FluentPaint.Core/Channels/Implementations/ChannelGetter.cs
+++ src/FluentPaint.Core/Channels/Implementations/ChannelGetter.cs
@@ -3,8 +3,19 @@ using SkiaSharp;
 
 namespace FluentPaint.Core.Channels.Implementations;
 
+/// <summary>
+/// Provides method to get one or many color channels of pictures.
+/// </summary>
 public class ChannelGetter : IChannelGetter
 {
+    /// <summary>
+    /// Get picture in provided color channels.
+    /// </summary>
+    /// <param name="bitmap"> <see cref="SKBitmap"/> Bitmap containing all pixels of the picture. </param>
+    /// <param name="channels"> Channels in which the image will be displayed. </param>
+    /// <returns>
+    /// Bitmap with pixels with correct color according to provided channels.
+    /// </returns>
     public SKBitmap GetChannels(SKBitmap bitmap, ColorChannels channels)
     {
         var modifiedBitmap = new SKBitmap(bitmap.Width, bitmap.Height);
