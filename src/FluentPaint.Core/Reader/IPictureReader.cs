5de02a1acdf50b5af96748ab1cdd841147e765e8
FuchsbauFox <vovasai@mail.ru>
2022-10-29 17:44:07 +0300

    fix: change comment in reader

--- src/FluentPaint.Core/Reader/IPictureReader.cs
+++ src/FluentPaint.Core/Reader/IPictureReader.cs
@@ -11,8 +11,8 @@ public interface IPictureReader
     /// Read picture from file.
     /// </summary>
     /// <param name="fileStream"> file with picture. </param>
-    /// <param name="width"> picture width, the number of bytes in the row. </param>
-    /// <param name="height"> picture height, the number of bytes in the column. </param>
+    /// <param name="width"> picture width, which is the number of bytes in the row. </param>
+    /// <param name="height"> picture height, which is the number of bytes in the column. </param>
     /// <returns>filled bitmap. </returns>
     SKBitmap ReadImageData(FileStream fileStream, int width, int height);
 }
