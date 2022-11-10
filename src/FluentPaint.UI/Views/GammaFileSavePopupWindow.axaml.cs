21973060e12218f37ea355f1625db6de851aef9d
Bazalii Ivan <bazalijivan2002@yandex.ru>
2022-11-10 23:33:44 +0300

    feat: realise GammaFileSavePopupWindow

---
+++ src/FluentPaint.UI/Views/GammaFileSavePopupWindow.axaml.cs
@@ -0,0 +1,32 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonia.Markup.Xaml;
+
+namespace FluentPaint.UI.Views;
+
+public partial class GammaFileSavePopupWindow : Window
+{
+    public GammaFileSavePopupWindow()
+    {
+        InitializeComponent();
+#if DEBUG
+        this.AttachDevTools();
+#endif
+    }
+
+    private void InitializeComponent()
+    {
+        AvaloniaXamlLoader.Load(this);
+#if DEBUG
+        this.AttachDevTools();
+#endif
+    }
+    
+    private void OnOkButtonClickCommand(object sender, RoutedEventArgs e)
+    {
+        var userInput = this.FindControl<TextBox>("SavingGammaFilePath").Text;
+
+        Close(userInput);
+    }
+}
