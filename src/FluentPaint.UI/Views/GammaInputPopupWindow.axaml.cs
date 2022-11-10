5ea4dccda6640df43f46952e71597e19419a10e6
Bazalii Ivan <bazalijivan2002@yandex.ru>
2022-11-10 23:33:32 +0300

    feat: realise GammaInputPopupWindow

---
+++ src/FluentPaint.UI/Views/GammaInputPopupWindow.axaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Globalization;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonia.Markup.Xaml;
+
+namespace FluentPaint.UI.Views;
+
+public partial class GammaInputPopupWindow : Window
+{
+    public GammaInputPopupWindow()
+    {
+        InitializeComponent();
+#if DEBUG
+        this.AttachDevTools();
+#endif
+    }
+
+    private void InitializeComponent()
+    {
+        AvaloniaXamlLoader.Load(this);
+#if DEBUG
+        this.AttachDevTools();
+#endif
+    }
+    
+    private void OnOkButtonClickCommand(object sender, RoutedEventArgs e)
+    {
+        var userInput = this.FindControl<TextBox>("GammaValue").Text;
+
+        Close(float.Parse(userInput, CultureInfo.InvariantCulture.NumberFormat));
+    }
+}
