34d88503611f9afa09d8810ce21bcb114f32ffb3
Bazalii Ivan <bazalijivan2002@yandex.ru>
2022-11-16 13:45:20 +0300

    feat: assign JpegReader and JpegWriter

---
+++ src/FluentPaint.Core/Pictures/Readers/Implementations/JpegReader.cs
@@ -0,0 +1,11 @@
+﻿using SkiaSharp;
+
+namespace FluentPaint.Core.Pictures.Readers.Implementations;
+
+public class JpegReader : IPictureReader
+{
+    public SKBitmap ReadImageData(FileStream fileStream, int width, int height)
+    {
+        return new SKBitmap();
+    }
+}
---
+++ src/FluentPaint.Core/Pictures/Writers/Implementations/JpegWriter.cs
@@ -0,0 +1,10 @@
+﻿using SkiaSharp;
+
+namespace FluentPaint.Core.Pictures.Writers.Implementations;
+
+public class JpegWriter : IPictureWriter
+{
+    public void WriteImageData(FileStream fileStream, SKBitmap bitmap)
+    {
+    }
+}
