82862c70cfb4bb76f48473a77e59cd8c7145313a
Bazalii Ivan <bazalijivan2002@yandex.ru>
2022-11-16 16:24:10 +0300

    feat: improve PictureHandler
    - Move ReadLine to PnmHeaderReader
    - Move WriteLine to PnmHeaderWriter
    - Change logic of Read and Write to make them more abstract

--- src/FluentPaint.Core/Pictures/Handlers/Implementations/PictureHandler.cs
+++ src/FluentPaint.Core/Pictures/Handlers/Implementations/PictureHandler.cs
@@ -1,5 +1,3 @@
-using System.Text;
-using FluentPaint.Core.Enums;
 using SkiaSharp;
 
 namespace FluentPaint.Core.Pictures.Handlers.Implementations;
@@ -23,23 +21,11 @@ public class PictureHandler : IPictureHandler
     {
         var fileStream = new FileStream(filePath, FileMode.Open, FileAccess.Read, FileShare.Read);
 
-        var type = ReadLine(fileStream);
+        var type = PictureFactory.GetType(filePath);
 
-        var line = ReadLine(fileStream);
-        var parameters = line.Split(new[] { ' ' });
-        var width = int.Parse(parameters[0]);
-        var height = int.Parse(parameters[1]);
+        var reader = PictureFactory.GetReader(type);
 
-        var brightness = int.Parse(ReadLine(fileStream));
-
-        if (brightness != 255)
-        {
-            throw new Exception("Brightness should be 255!");
-        }
-
-        var reader = PictureFactory.GetReader(PictureFactory.GetType(type));
-
-        return reader.ReadImageData(fileStream, width, height);
+        return reader.ReadImageData(fileStream);
     }
 
     /// <summary>
@@ -52,58 +38,11 @@ public class PictureHandler : IPictureHandler
     /// </exception>
     public void Write(string filePath, SKBitmap bitmap)
     {
-        var extension = filePath.Substring(filePath.Length - 4, 4).ToLower();
-
-        var type = extension switch
-        {
-            ".pgm" => PictureType.P5,
-            ".ppm" => PictureType.P6,
-            ".pnm" => PictureType.P6,
-            _ => throw new Exception(
-                "Error: This file type is not supported, .ppm .pgm is expected (.pnm will write as p6)")
-        };
+        var type = PictureFactory.GetType(filePath);
 
         var fileStream = new FileStream(filePath, FileMode.Create, FileAccess.Write, FileShare.None);
         var writer = PictureFactory.GetWriter(type);
 
-        WriteLine(fileStream, type.ToString());
-        WriteLine(fileStream, bitmap.Width + " " + bitmap.Height);
-        WriteLine(fileStream, "255");
-
-        writer.WriteImageData(fileStream, bitmap);
-    }
-
-    /// <summary>
-    /// Reads line from provided filestream.
-    /// </summary>
-    /// <param name="fileStream"> <see cref="Stream"/> that contains information to read. </param>
-    /// <returns>
-    /// Line that was read from provided filestream.
-    /// </returns>
-    private static string ReadLine(Stream fileStream)
-    {
-        var binaryReader = new BinaryReader(fileStream);
-        var stringBuilder = new StringBuilder();
-        var current = binaryReader.ReadByte();
-
-        while (current != '\n')
-        {
-            stringBuilder.Append((char) current);
-            current = binaryReader.ReadByte();
-        }
-
-        return stringBuilder.ToString();
-    }
-
-    /// <summary>
-    /// Writes line to provided filestream.
-    /// </summary>
-    /// <param name="fileStream"> <see cref="Stream"/> that is used for writing information. </param>
-    /// <param name="line"> String that will be written to filestream. </param>
-    private static void WriteLine(Stream fileStream, string line)
-    {
-        var binaryWriter = new BinaryWriter(fileStream);
-
-        binaryWriter.Write(Encoding.ASCII.GetBytes(line + "\n"));
+        writer.WriteImageData(fileStream, bitmap, type);
     }
 }
