cb8cdde0edee993074ebe5ad94bf253161a6f194
Bazalii Ivan <bazalijivan2002@yandex.ru>
2022-11-17 23:44:56 +0300

    refactor: improve OrderedDithering
    - Realise new method CalculateNewPixelComponent to remove duplicating code
    - Add spaces, blank lines and cleanup files
    - Rename matrix to _matrix, make it readonly and improve its initialisation

--- src/FluentPaint.Core/Dithering/Implementations/OrderedDithering.cs
+++ src/FluentPaint.Core/Dithering/Implementations/OrderedDithering.cs
@@ -4,7 +4,7 @@ namespace FluentPaint.Core.Dithering.Implementations;
 
 public class OrderedDithering : IDithering
 {
-    private double[,] matrix = new double[8, 8]
+    private readonly double[,] _matrix =
     {
         { 0, 32, 8, 40, 2, 34, 10, 42 },
         { 48, 16, 56, 24, 50, 18, 58, 26 },
@@ -19,30 +19,24 @@ public class OrderedDithering : IDithering
     public SKBitmap Dithering(SKBitmap bitmap, int bitDepth)
     {
         var resultBitmap = new SKBitmap(bitmap.Width, bitmap.Height);
-        
+
         for (var y = 0; y < bitmap.Height; y++)
         {
             for (var x = 0; x < bitmap.Width; x++)
             {
                 var pixel = bitmap.GetPixel(x, y);
-                
-                var red = matrix[x % 7, y % 7] / 64 * 255 > pixel.Red
-                    ? GetLeftColor(pixel.Red, bitDepth)
-                    : GetRightColor(pixel.Red, bitDepth);
-                var green = matrix[x % 7, y % 7] / 64 * 255 > pixel.Green
-                    ? GetLeftColor(pixel.Green, bitDepth)
-                    : GetRightColor(pixel.Green, bitDepth);
-                var blue = matrix[x % 7, y % 7] / 64 * 255 > pixel.Blue
-                    ? GetLeftColor(pixel.Blue, bitDepth)
-                    : GetRightColor(pixel.Blue, bitDepth);
-                
+
+                var red = CalculateNewPixelComponent(_matrix[x % 7, y % 7], pixel.Red, bitDepth);
+                var green = CalculateNewPixelComponent(_matrix[x % 7, y % 7], pixel.Green, bitDepth);
+                var blue = CalculateNewPixelComponent(_matrix[x % 7, y % 7], pixel.Blue, bitDepth);
+
                 resultBitmap.SetPixel(x, y, new SKColor(red, green, blue));
             }
         }
 
         return resultBitmap;
     }
-    
+
     private byte GetLeftColor(byte pixelColor, int bitDepth)
     {
         double color = 0;
@@ -54,7 +48,7 @@ public class OrderedDithering : IDithering
             color += 255 / (Math.Pow(2, bitDepth) - 1);
         }
 
-        return (byte)color;
+        return (byte) color;
     }
 
     private byte GetRightColor(byte pixelColor, int bitDepth)
@@ -68,6 +62,13 @@ public class OrderedDithering : IDithering
             color += 255 / (Math.Pow(2, bitDepth) - 1);
         }
 
-        return (byte)(color + 255 / (Math.Pow(2, bitDepth) - 1));
+        return (byte) (color + 255 / (Math.Pow(2, bitDepth) - 1));
+    }
+
+    private byte CalculateNewPixelComponent(double matrixElement, byte pixelComponent, int bitDepth)
+    {
+        return matrixElement / 64 * 255 > pixelComponent
+            ? GetLeftColor(pixelComponent, bitDepth)
+            : GetRightColor(pixelComponent, bitDepth);
     }
 }
