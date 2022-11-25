4355820ed13dd163d781200a1c3719c37adfeb78
Bazalii Ivan <bazalijivan2002@yandex.ru>
2022-11-25 16:03:28 +0300

    feat: realise Point

---
+++ src/FluentPaint.Core/Pictures/Readers/Models/Point.cs
@@ -0,0 +1,8 @@
+﻿namespace FluentPaint.Core.Pictures.Readers.Models;
+
+public class Point
+{
+    public int Row { get; set; }
+    public int Column { get; set; }
+    public bool DiagonalVector { get; set; }
+}
