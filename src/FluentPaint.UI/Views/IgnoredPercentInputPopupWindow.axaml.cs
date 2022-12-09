b8091d37e986c64d614ab9020fdb0e8625c26ab8
Bazalii Ivan <bazalijivan2002@yandex.ru>
2022-12-09 19:03:26 +0300

    feat: realise IgnoredPercentInputPopupWindow model

---
+++ src/FluentPaint.UI/Views/IgnoredPercentInputPopupWindow.axaml.cs
@@ -0,0 +1,34 @@
+﻿using System.Globalization;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonia.Markup.Xaml;
+
+namespace FluentPaint.UI.Views;
+
+public partial class IgnoredPercentInputPopupWindow : Window
+{
+    public IgnoredPercentInputPopupWindow()
+    {
+        InitializeComponent();
+
+#if DEBUG
+        this.AttachDevTools();
+#endif
+    }
+
+    private void InitializeComponent()
+    {
+        AvaloniaXamlLoader.Load(this);
+#if DEBUG
+        this.AttachDevTools();
+#endif
+    }
+    
+    private void OnOkButtonClickCommand(object sender, RoutedEventArgs e)
+    {
+        var userInput = this.FindControl<TextBox>("IgnoredPercent").Text;
+
+        Close(double.Parse(userInput, CultureInfo.InvariantCulture.NumberFormat));
+    }
+}
