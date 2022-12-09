49ffcb192ff026bde524179f384c7ffa03f469f5
Bazalii Ivan <bazalijivan2002@yandex.ru>
2022-12-09 17:37:46 +0300

    feat: realise ColorChannelsParser

---
+++ src/FluentPaint.Core/Parsers/ColorChannelsParser.cs
@@ -0,0 +1,20 @@
+﻿namespace FluentPaint.Core.Parsers;
+
+public class ColorChannelsParser
+{
+    public List<string> Parse(string colorChannels)
+    {
+        if (!colorChannels.Contains("And"))
+        {
+            return colorChannels == "All"
+                ? new List<string> { "First", "Second", "Third" }
+                : new List<string> { colorChannels };
+        }
+
+        var andIndex = colorChannels.IndexOf("And", StringComparison.Ordinal);
+        var firstValue = colorChannels[..andIndex];
+        var secondValue = colorChannels[(andIndex + 3)..];
+
+        return new List<string> { firstValue, secondValue };
+    }
+}
