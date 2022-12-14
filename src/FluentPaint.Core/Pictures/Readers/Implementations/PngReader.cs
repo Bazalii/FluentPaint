beb444cb220bbd43cb55915cb86c67d805a09e56
Bazalii Ivan <bazalijivan2002@yandex.ru>
2022-12-14 13:53:57 +0300

    feat: assign PngReader and PngWriter

---
+++ src/FluentPaint.Core/Pictures/Readers/Implementations/PngReader.cs
@@ -0,0 +1,11 @@
+﻿using SkiaSharp;
+
+namespace FluentPaint.Core.Pictures.Readers.Implementations;
+
+public class PngReader : IPictureReader
+{
+    public SKBitmap ReadImageData(FileStream fileStream)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ src/FluentPaint.Core/Pictures/Writers/Implementations/PngWriter.cs
@@ -0,0 +1,12 @@
+﻿using FluentPaint.Core.Enums;
+using SkiaSharp;
+
+namespace FluentPaint.Core.Pictures.Writers.Implementations;
+
+public class PngWriter : IPictureWriter
+{
+    public void WriteImageData(FileStream fileStream, SKBitmap bitmap, PictureType type)
+    {
+        throw new NotImplementedException();
+    }
+}
