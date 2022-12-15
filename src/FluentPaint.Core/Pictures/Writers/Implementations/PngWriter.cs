dfe9a0924b12a318e3eb218e487cfdbbf37d93d7
Bazalii Ivan <bazalijivan2002@yandex.ru>
2022-12-15 14:06:58 +0300

    feat: realise WriteHeader in PngWriter

--- src/FluentPaint.Core/Pictures/Writers/Implementations/PngWriter.cs
+++ src/FluentPaint.Core/Pictures/Writers/Implementations/PngWriter.cs
@@ -1,12 +1,39 @@
-﻿using FluentPaint.Core.Enums;
+﻿using System.Text;
+using FluentPaint.Core.Enums;
 using SkiaSharp;
 
 namespace FluentPaint.Core.Pictures.Writers.Implementations;
 
 public class PngWriter : IPictureWriter
 {
+    private const byte BitDepth = 8;
+    private const byte ColorType = 2;
+    private const byte CompressionMethod = 0;
+    private const byte FilterMethod = 0;
+    private const byte InterlaceMethod = 0;
+    
+    private FileStream _fileStream;
+    private SKBitmap _bitmap;
+    
     public void WriteImageData(FileStream fileStream, SKBitmap bitmap, PictureType type)
     {
-        throw new NotImplementedException();
+        _fileStream = fileStream;
+        _bitmap = bitmap;
+        
+        WriteHeader();
+    }
+    
+    private void WriteHeader()
+    {
+        var sectionName = new ASCIIEncoding().GetBytes("IHDR");
+        
+        _fileStream.Write(sectionName);
+        _fileStream.Write(BitConverter.GetBytes(_bitmap.Width));
+        _fileStream.Write(BitConverter.GetBytes(_bitmap.Height));
+        _fileStream.WriteByte(BitDepth);
+        _fileStream.WriteByte(ColorType);
+        _fileStream.WriteByte(CompressionMethod);
+        _fileStream.WriteByte(FilterMethod);
+        _fileStream.WriteByte(InterlaceMethod);
     }
 }
