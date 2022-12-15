c2a0b4505d9ba29fe880f23b1c87b0ea97c35bda
Bazalii Ivan <bazalijivan2002@yandex.ru>
2022-12-15 17:40:38 +0300

    feat: improve PngWriter
    - Realise WriteSignature
    - Change logic of WriteHeader

--- src/FluentPaint.Core/Pictures/Writers/Implementations/PngWriter.cs
+++ src/FluentPaint.Core/Pictures/Writers/Implementations/PngWriter.cs
@@ -1,5 +1,7 @@
 ﻿using System.Text;
 using FluentPaint.Core.Enums;
+using ICSharpCode.SharpZipLib.Checksum;
+using ICSharpCode.SharpZipLib.Zip.Compression.Streams;
 using SkiaSharp;
 
 namespace FluentPaint.Core.Pictures.Writers.Implementations;
@@ -11,29 +13,51 @@ public class PngWriter : IPictureWriter
     private const byte CompressionMethod = 0;
     private const byte FilterMethod = 0;
     private const byte InterlaceMethod = 0;
-    
+    private const byte BytesPerPixel = 3;
+
     private FileStream _fileStream;
     private SKBitmap _bitmap;
-    
+
     public void WriteImageData(FileStream fileStream, SKBitmap bitmap, PictureType type)
     {
         _fileStream = fileStream;
         _bitmap = bitmap;
-        
+
+        WriteSignature();
         WriteHeader();
     }
-    
+
+    private void WriteSignature()
+    {
+        var signature = new byte[] { 137, 80, 78, 71, 13, 10, 26, 10 };
+        
+        _fileStream.Write(signature);
+    }
+
     private void WriteHeader()
     {
         var sectionName = new ASCIIEncoding().GetBytes("IHDR");
-        
-        _fileStream.Write(sectionName);
-        _fileStream.Write(BitConverter.GetBytes(_bitmap.Width));
-        _fileStream.Write(BitConverter.GetBytes(_bitmap.Height));
-        _fileStream.WriteByte(BitDepth);
-        _fileStream.WriteByte(ColorType);
-        _fileStream.WriteByte(CompressionMethod);
-        _fileStream.WriteByte(FilterMethod);
-        _fileStream.WriteByte(InterlaceMethod);
+
+        var section = new List<byte>();
+
+        section.AddRange(sectionName);
+        section.AddRange(BitConverter.GetBytes(_bitmap.Width).Reverse());
+        section.AddRange(BitConverter.GetBytes(_bitmap.Height).Reverse());
+        section.Add(BitDepth);
+        section.Add(ColorType);
+        section.Add(CompressionMethod);
+        section.Add(FilterMethod);
+        section.Add(InterlaceMethod);
+
+        var sectionLength = BitConverter.GetBytes(section.Count - 4).Reverse();
+
+        var cyclicRedundancyCodeCalculator = new Crc32();
+        cyclicRedundancyCodeCalculator.Update(section.ToArray());
+
+        var cyclicRedundancyCode = BitConverter.GetBytes((int) cyclicRedundancyCodeCalculator.Value).Reverse();
+
+        _fileStream.Write(sectionLength.ToArray());
+        _fileStream.Write(section.ToArray());
+        _fileStream.Write(cyclicRedundancyCode.ToArray());
     }
 }
