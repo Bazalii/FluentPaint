2899bc8ff260a772e449ed8532eb35b1d5ad0b4e
Bazalii Ivan <bazalijivan2002@yandex.ru>
2022-12-16 13:02:56 +0300

    feat: realise FluentBitmap

---
+++ src/FluentPaint.Core/Pictures/FluentBitmap.cs
@@ -0,0 +1,12 @@
+﻿using SkiaSharp;
+
+namespace FluentPaint.Core.Pictures;
+
+public class FluentBitmap : SKBitmap
+{
+    public FluentBitmap(int width, int height) : base(width, height)
+    {
+    }
+    
+    public float Gamma { get; set; } = 1.0f;
+}
