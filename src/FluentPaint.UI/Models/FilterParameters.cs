6d44fe98c60ab9aa85250a0c34e4d312f71d9017
Bazalii Ivan <bazalijivan2002@yandex.ru>
2022-12-23 13:17:19 +0300

    feat: realise FilterParameters

---
+++ src/FluentPaint.UI/Models/FilterParameters.cs
@@ -0,0 +1,10 @@
+﻿namespace FluentPaint.UI.Models;
+
+public class FilterParameters
+{
+    public float Sigma { get; set; }
+    public float Sharpness { get; set; }
+    public int Radius { get; set; }
+    public int Limit { get; set; }
+    public byte Threshold { get; set; }
+}
